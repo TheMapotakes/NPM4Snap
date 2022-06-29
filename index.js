7dd381a605316f04b12733bc142309562a422a7c
TheMapotakes test <themapotakestest@gmail.com>
2022-06-29 11:18:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,7 @@
 SnapExtensions.primitives.set(
-  'SciS_SetSciSnapLogo(imagelogo)',
-  function (imagelogo) {
-    IDE_Morph.prototype.createLogo = function () {
-      var myself = this;
-
-      if (this.logo) {
-        this.logo.destroy();
-      }
-
-      this.logo = new Morph();
-
-      this.logo.texture = imagelogo;
-      this.logo.render = function (ctx) {
-        var gradient = ctx.createLinearGradient(
-          0,
-          0,
-          this.width(),
-          0
-          );
-        gradient.addColorStop(0, 'black');
-        gradient.addColorStop(0.5, myself.frameColor.toString());
-        ctx.fillStyle = MorphicPreferences.isFlat ?
-          myself.frameColor.toString() : gradient;
-        ctx.fillRect(0, 0, this.width(), this.height());
-        if (this.cachedTexture) {
-          this.renderCachedTexture(ctx);
-        } else if (this.texture) {
-          this.renderTexture(this.texture, ctx);
-        }
-      };
-
-      this.logo.renderCachedTexture = function (ctx) {
-        ctx.drawImage(
-          this.cachedTexture,
-          5,
-          Math.round((this.height() - this.cachedTexture.height) / 2)
-          );
-        this.changed();
-      };
-
-      this.logo.mouseClickLeft = function () {
-        myself.snapMenu();
-      };
-
-      this.logo.color = BLACK;
-      this.logo.setExtent(new Point(200, 28)); // dimensions are fixed
-      this.add(this.logo);
-    };
-    var stage = this.parentThatIsA(StageMorph),
-      ide = stage.parentThatIsA(IDE_Morph),
-      world = stage.parentThatIsA(WorldMorph);
-    ide.createLogo();
-    ide.createControlBar();
-    ide.fixLayout();
+  'install_package(package)',
+  function (package) {
+    var package = require(package);
+    return package
   }
 );
